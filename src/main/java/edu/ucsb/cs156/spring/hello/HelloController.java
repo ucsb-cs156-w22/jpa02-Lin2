90d767e1adf6fbcbd5e44bf084b009523f3e7ad6
Rose Lin <tianyi_lin@ucsb.edu>
2022-01-11 13:53:19 -0800

    rl - changed underscore in email to hyphen

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from tianyi_lin!";
+        return "Greetings from tianyi-lin!";
     }
     
 }
